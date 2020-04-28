4f3741ea0fd9550747932b035b92772450522e7e
Daniel Sasser <danielbsasser@gmail.com>
2020-04-28 02:20:48 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     @authors = Author.all
     @authors = Author.all
-   
+
     # filter the @posts list based on user input
     if !params[:author].blank?
       @posts = Post.where(author: params[:author])
